544e4788b9af3478a7c4d2bbd3999fc1492b0546
Yev <yevyork@gmail.com>
2019-12-17 09:45:26 -0500

    changed folder structure

--- recipe-helper/src/components/Gallery.jsx
+++ recipe-helper/src/components/Gallery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
+import './styles/Gallery.css'
 import { news } from "../seed";
 
 
